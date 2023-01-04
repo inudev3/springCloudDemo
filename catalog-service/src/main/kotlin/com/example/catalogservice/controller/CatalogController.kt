defb3897312f1456e7a1e4a6de24003f4983d030
Inu Jung <inust33@mobility42.io>
2023-01-04 15:56:07 +0900

    ..

--- catalog-service/src/main/kotlin/com/example/catalogservice/controller/CatalogController.kt
+++ catalog-service/src/main/kotlin/com/example/catalogservice/controller/CatalogController.kt
@@ -1,4 +1,27 @@
 package com.example.catalogservice.controller
 
-class CatalogController {
+import com.example.catalogservice.Mapper.ModelMapper
+import com.example.catalogservice.entity.CatalogEntity
+import com.example.catalogservice.service.CatalogService
+import com.example.catalogservice.vo.ResponseCatalog
+import org.springframework.core.env.Environment
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/catalog-service")
+class CatalogController (val mapper: ModelMapper, val environment: Environment, val catalogService: CatalogService){
+    @GetMapping("/health-check")
+    fun status():String{
+
+        return "health Check"
+    }
+    @GetMapping("/catalogs")
+    fun getCatalogs(): ResponseEntity<List<ResponseCatalog>> =catalogService.getAllCatalogs().map{
+        mapper.mapper<CatalogEntity,ResponseCatalog>(it)
+    }.let{ ResponseEntity.status(HttpStatus.OK).body(it)}
+
 }
